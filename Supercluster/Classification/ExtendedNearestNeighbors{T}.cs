db8a7729412bdaed781c4f37ca8322472544dde0
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-05-26 08:11:41 +0000

    save point

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -10,4 +10,8 @@
     /// Bo Tang; Haibo He, "ENN: Extended Nearest Neighbor Method for Pattern Recognition [Research Frontier]," in Computational Intelligence Magazine, IEEE , vol.10, no.3, pp.52-60, Aug. 2015 doi: 10.1109/MCI.2015.2437512
     /// </summary>
     /// <typeparam name="T">The data-type of the data points to be classified.</typeparam>
-    public c
+    public class ExtendedNearestNeighbors<T> : ISupervisedClassifier<T>
+    {
+        /// <summary>
+        /// An array containing the original training data.
+   
