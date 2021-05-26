65f60d56d24ab2289c5c030e4ea57004c6bebe88
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-05-26 06:11:38 +0000

    nouns

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -8,3 +8,6 @@
     /// <summary>
     /// The Extended Nearest Neighbors learning algorithm is a variation of the kNN learning algorithm which classification decisions are made in such a way that they maximize intra-class coherence.
     /// Bo Tang; Haibo He, "ENN: Extended Nearest Neighbor Method for Pattern Recognition [Research Frontier]," in Computational Intelligence Magazine, IEEE , vol.10, no.3, pp.52-60, Aug. 2015 doi: 10.1109/MCI.2015.2437512
+    /// </summary>
+    /// <typeparam name="T">The data-type of the data points to be classified.</typeparam>
+    public c
