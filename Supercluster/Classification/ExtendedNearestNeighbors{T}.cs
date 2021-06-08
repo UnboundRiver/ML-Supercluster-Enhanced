0d1183c7c56f29b858e541f3fd8d11352c737425
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-06-08 21:13:28 +0000

    tg3: Allow for recieve of full-size 8021AD frames

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -99,4 +99,5 @@
                     // calculates the distances for each Point 
                     var testingMuDistances = distances.WithIndexes(indicies).Select(x => x[1]).ToArray();
 
-                    // o
+                    // only get the k-th nearest neighbor
+                    var trainingMuDis = this.WeightedKNNDistance.WithIndexes(indicies).Select(x => 
