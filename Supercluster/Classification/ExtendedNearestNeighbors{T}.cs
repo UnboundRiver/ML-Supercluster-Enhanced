83d8bf01abbd3d11d7e5c3f71076a00b3218ce55
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-07-05 18:16:39 +0000

    Bump plugin version

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -174,4 +174,8 @@
             var hitNumKNN = new int[numClass];
             for (int j = 0; j < numClass; j++)
             {
-                hitNumK
+                hitNumKNN[j] = distances.Take(this.K).Count(d => (int)d[2] == j);
+            }
+
+            // variables defined in matlab program
+ 
