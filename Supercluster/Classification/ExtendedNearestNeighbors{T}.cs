4bdf54b98d31f2d69fe329e24f6596ebce1215c8
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-07-08 12:14:36 +0000

    Format readme

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -187,4 +187,6 @@
             {
                 // TODO: Optimize the shit out of this
 
-                /
+                // Get all the indexes of the current class
+                var indicies = this.trainingOutputs.WhereIndex(l => l == j).ToArray();
+        
