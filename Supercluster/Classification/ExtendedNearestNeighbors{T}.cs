178e4c729f7cceb15e3d81fab746aeef5d194f25
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-07-11 17:17:13 +0000

    zwave

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -195,4 +195,7 @@
                 var testingMuDistances = distances.WithIndexes(indicies).Select(x => x[1]).ToArray();
 
                 // only get the k-th nearest neighbor
-                var trainingMuDis = this.WeightedKNNDistance.WithIndexes(indicies).Select(x => x[this.K - 1]).ToArray(
+                var trainingMuDis = this.WeightedKNNDistance.WithIndexes(indicies).Select(x => x[this.K - 1]).ToArray();
+                var trainingMuClass = this.WeightedKNNLabels.WithIndexes(indicies).Select(x => x[this.K - 1]).ToArray();
+
+     
