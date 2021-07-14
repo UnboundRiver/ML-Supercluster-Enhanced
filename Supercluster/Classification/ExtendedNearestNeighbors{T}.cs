f791b60c83a6fbbfb18e69890a82811b7d440b6b
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-07-14 16:14:38 +0000

    shell

--- Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
+++ Supercluster/Classification/ExtendedNearestNeighbors{T}.cs
@@ -214,4 +214,6 @@
             {
                 var deltaNumSame = numTrainingNN[j] - numSameTraining[j];
                 var difTmp = numSameTraining.ArrayDivide(numTrainingEachClass.Select(x => (double)(x * this.K)).ToArray());
-     
+                var deltaNumDif = difTmp.Sum() - (numSameTraining[j] / ((double)(numTrainingEachClass[j] * this.K)));
+
+                tsEnn
