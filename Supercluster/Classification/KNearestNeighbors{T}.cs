12bc89e75d1a0e21a5d1d3e15df7ff36f1cd00f8
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-08-11 22:27:19 +0000

    Updated source

--- Supercluster/Classification/KNearestNeighbors{T}.cs
+++ Supercluster/Classification/KNearestNeighbors{T}.cs
@@ -16,4 +16,5 @@
     /// <para> The <i>k</i> nearest neighbors learning algorithm (kNN) is a classification algorithm. To determine class membership of a unseen input the algorithm computes the <i>k</i> nearest neighbors in the learned feature space 
     /// and then chooses the class which is most common among the <i>k</i> neighbors.</para>
     /// 
-    /// <para> The picture below is of the output of the kNN algorithm k=3 in a 2-dimensional feature s
+    /// <para> The picture below is of the output of the kNN algorithm k=3 in a 2-dimensional feature space with 2 classes. The blue and red dots are point belonging to class 1 and class 2 which were learned during model training.
+    ///
