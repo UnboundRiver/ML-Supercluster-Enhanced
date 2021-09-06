0185937adb2a4997bf33d4cc4b76505484a01d27
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-09-06 17:49:44 +0000

    Revert "Update CONTRIBUTING.md"

--- Supercluster/Classification/KNearestNeighbors{T}.cs
+++ Supercluster/Classification/KNearestNeighbors{T}.cs
@@ -97,4 +97,6 @@
         /// <param name="point">The point to add to the model</param>
         /// <param name="label">The class label of the given <paramref name="point"/></param>
         /// <exception cref="ArgumentException">Thrown if the label of the point does not exist in the current model.</exception>
- 
+        public void Train(T point, int label)
+        {
+            var pointIndex = this.internalDataStructure.Add(poi
