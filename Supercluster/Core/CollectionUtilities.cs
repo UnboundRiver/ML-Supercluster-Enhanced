a2d282fa432d78d5c956eda256812ce20595610a
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-09-17 18:56:19 +0000

    add Rakefile

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -4,4 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
 
-    /// 
+    /// <summary>
+    /// A utility class containing various extension methods to be used with the .NET collection classes.
+    /// </summa
