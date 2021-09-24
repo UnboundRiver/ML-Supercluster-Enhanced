91f4635e41157f904f63c78fc94bc6db8ee4fa77
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-09-24 06:27:00 +0000

    Revert "adapt Rakefile to submit to correct build service project in maintenance branch SLE-12-GA"

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -46,4 +46,5 @@
         /// <summary>
         /// Invokes a transform function on each element of <paramref name="source"/> and returns the index of the minimum <typeparamref name="TResult"/> value.
         /// </summary>
-        /// <typeparam name="TSource">The 
+        /// <typeparam name="TSource">The type of the elements in <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the result of the <paramref name="sel
