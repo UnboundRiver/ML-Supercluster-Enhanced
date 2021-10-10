1c40e9a502c9272389ea13e68edcd2bb1ca1a598
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-10-10 09:35:19 +0000

    1.0.0-alpha.1

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -144,4 +144,5 @@
         /// <summary>
         /// Returns the indexes of the elements in <paramref name="source"/> satisfying the predicate.
         /// </summary>
-        /// <typeparam name="TSource">The type of the 
+        /// <typeparam name="TSource">The type of the elements in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to filter based o
