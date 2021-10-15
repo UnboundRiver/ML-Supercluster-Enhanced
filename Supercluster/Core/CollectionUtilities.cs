65384e7f81769a6d96920200e4456fb3f179ffe4
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-10-15 22:49:47 +0000

    target

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -172,4 +172,5 @@
         /// Returns the index of the first element of a sequence that satisfies the given predicate.
         /// </summary>
         /// <typeparam name="TSource">The type of the elements in <paramref name="source"/>.</typeparam>
-        /// <param name="source">A sequence of values to find the first element sa
+        /// <param name="source">A sequence of values to find the first element satisfying the <paramref name="predicate"/>.</param>
+        /// <param name="predicate">A predicate to test elements in <paramre
