3b8cf79d84cff02d12403e9abe30fbdaae620b40
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-10-27 17:23:56 +0000

    mnt: Change the default remount atime from relatime to the existing value

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -256,4 +256,5 @@
         /// <summary>
         /// Returns the index of the last element of a sequence that satisfies the given predicate.
         /// </summary>
-        /// <typeparam name="TSource">The type of the elements in <paramref name=
+        /// <typeparam name="TSource">The type of the elements in <paramref name="source"/>.</typeparam>
+        /// <param name="source">A sequence of values to find the last element satisfying the <param
