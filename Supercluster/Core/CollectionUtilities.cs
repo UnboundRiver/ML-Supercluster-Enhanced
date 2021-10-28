f06727188dddd4d61965f27e61b26dcb1bd3ad3c
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-10-28 16:23:38 +0000

    Update cookbook.md

--- Supercluster/Core/CollectionUtilities.cs
+++ Supercluster/Core/CollectionUtilities.cs
@@ -261,4 +261,6 @@
         /// <param name="predicate">A predicate to test elements in <paramref name="source"/>.</param>
         /// <returns>The index of the last element in <paramref name="source"/> that satisfy <paramref name="predicate"/>.
         /// If no element satisfies the <paramref name="predicate"/> then -1 is returned.</returns>
-    
+        public static int LastIndexOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            var la
