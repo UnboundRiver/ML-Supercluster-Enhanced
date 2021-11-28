2cc98a7d5a6f0eeceb976fafd0f9cff5e1e9c671
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-11-28 09:00:59 +0000

    Fixing a Problem in display

--- Supercluster/Core/CoordinateSelector.cs
+++ Supercluster/Core/CoordinateSelector.cs
@@ -4,4 +4,7 @@
     using System.Linq.Expressions;
 
     /// <summary>
-   
+    /// A static utility class which provides methods for creating a Coordinate Selector.
+    /// </summary>
+    /// <remarks>
+    /// 
