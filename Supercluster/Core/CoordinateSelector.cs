66402eade8d3201cbf6d453644c7d211ea686a0e
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-11-28 14:49:54 +0000

    x86, efi: Don't use (U)EFI time services on 32 bit

--- Supercluster/Core/CoordinateSelector.cs
+++ Supercluster/Core/CoordinateSelector.cs
@@ -7,4 +7,5 @@
     /// A static utility class which provides methods for creating a Coordinate Selector.
     /// </summary>
     /// <remarks>
-    /// 
+    /// A "Coordinate Selector" is a <see cref="Func{T,TResult}"/> which accepts any type of object and returns and array of doubles.
+    /// This dou
