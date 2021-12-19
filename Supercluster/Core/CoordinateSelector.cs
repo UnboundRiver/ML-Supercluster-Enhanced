943ade80f974c301c7b10b60064d5d6d6d215d79
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2021-12-19 20:20:16 +0000

    fix mac build

--- Supercluster/Core/CoordinateSelector.cs
+++ Supercluster/Core/CoordinateSelector.cs
@@ -44,4 +44,6 @@
         /// Creates a coordinate selector that returns the value of a property or field with the name "Coordinate".
         /// </summary>
         /// <typeparam name="T">The type of the object for which to compile a point selector for.</typeparam>
-        /// <returns>A Func which can select a the coordinate from any insta
+        /// <returns>A Func which can select a the coordinate from any instance of <typeparamref name="T"/>.</returns>
+        public static Func<T, double[]> CreateDefaultCoordinateSelector<T>()
+        {
