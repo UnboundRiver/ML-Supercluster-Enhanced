5cedff49ca30ae226806c42f44c6e68e0b020030
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-01-09 17:03:17 +0000

    more problems

--- Supercluster/Core/ReverseComparer.cs
+++ Supercluster/Core/ReverseComparer.cs
@@ -4,4 +4,7 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /// An <see cref="IComparer"/> that reverses the result of the <see cref="Compar
+    /// An <see cref="IComparer"/> that reverses the result of the <see cref="Compare"/> function.
+    /// Used to reverse sort order.
+    /// </summary>
+    /// <typeparam name="T">The type of the object to be 
