d9d8c1a6646ff8188ddba482bb15e450594a1cac
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-01-09 06:16:05 +0000

    stuff I don't need to see

--- Supercluster/Core/DoubleEqualityComparer.cs
+++ Supercluster/Core/DoubleEqualityComparer.cs
@@ -41,4 +41,9 @@
             return true;
         }
 
-        public int GetHashCo
+        public int GetHashCode(double[] obj)
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ Supercluster/Core/ReverseComparer.cs
@@ -0,0 +1,3 @@
+﻿namespace Supercluster.Core
+{
+    using System.Co
