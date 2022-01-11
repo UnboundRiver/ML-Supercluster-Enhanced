418f1c682936efb41108096e174cac5ad8ebc46c
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-01-11 14:38:56 +0000

    Remove newlines

--- Supercluster/Core/ReverseComparer.cs
+++ Supercluster/Core/ReverseComparer.cs
@@ -7,4 +7,8 @@
     /// An <see cref="IComparer"/> that reverses the result of the <see cref="Compare"/> function.
     /// Used to reverse sort order.
     /// </summary>
-    /// <typeparam name="T">The type of the object to be 
+    /// <typeparam name="T">The type of the object to be prepared.</typeparam>
+    public class ReverseComparer<T> : IComparer<T>
+    {
+        /// <inheritdoc />
+        pu
