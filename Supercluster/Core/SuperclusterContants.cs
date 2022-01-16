5b62bd26a3b0f8c1d555b0555284120fe1d149b0
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-01-16 14:33:20 +0000

    - Initial release

--- Supercluster/Core/SuperclusterContants.cs
+++ Supercluster/Core/SuperclusterContants.cs
@@ -5,4 +5,6 @@
     /// </summary>
     public static class SuperclusterContants
     {
- 
+        /// <summary>
+        /// The tolerance used when comparing <see cref="double"/> values.
+        /// </summary
