d440a1d0cf9a8bc3d3801e3688694a7415d51df9
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-01-23 16:26:41 +0000

    ENH: First checkin

--- Supercluster/Core/SuperclusterContants.cs
+++ Supercluster/Core/SuperclusterContants.cs
@@ -14,4 +14,7 @@
         /// <summary>
         /// The name of the property or field searched for trying to detect a type's coordinate representation.
         /// </summary>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names must not contain underscore
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names must not contain underscore", Justification = "Library constant")]
+        public const string COORDINATE_FIELD_OR_PROPERTY_NAME = "Coordinate";
+    }
+}
