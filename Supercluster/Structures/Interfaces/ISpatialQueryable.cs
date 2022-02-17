f9f24921dd407914e71ea9cd471fc76df1466dc7
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-02-17 10:20:15 +0000

    migrating

--- Supercluster/Structures/Interfaces/ISpatialQueryable.cs
+++ Supercluster/Structures/Interfaces/ISpatialQueryable.cs
@@ -6,4 +6,9 @@
     /// An append only data structure that supports basic spatial access methods.
     /// </summary>
     /// <typeparam name="T">The type of the objects in the collection.</typeparam>
-    public interface ISpatialQueryabl
+    public interface ISpatialQueryable<T>
+    {
+        /// <summary>
+        /// Add the items to the current collection/
+        /// </summary>
+        /// <param name="it
