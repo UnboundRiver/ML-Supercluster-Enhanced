9565f48249efbd5e2f09d76e1293ca778d17538f
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-03-02 07:07:16 +0000

    Bump to version 1.1

--- Supercluster/Structures/Interfaces/ISpatialQueryable.cs
+++ Supercluster/Structures/Interfaces/ISpatialQueryable.cs
@@ -32,4 +32,7 @@
         IEnumerable<T> NearestNeighbors(T target, int k);
 
         /// <summary>
-        /// Searches for the <paramref name="k"/> nearest neighbors of the specified <paramref name
+        /// Searches for the <paramref name="k"/> nearest neighbors of the specified <paramref name="target"/>.
+        /// Note that the target point may or may not be in the collection.
+        /// </summary>
+        /// <param name="target">
