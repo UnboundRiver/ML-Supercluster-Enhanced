c848285c949a491c48359538ff1685073496c033
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-03-13 10:09:14 +0000

    wiki changes

--- Supercluster/Structures/Interfaces/ISpatialQueryable.cs
+++ Supercluster/Structures/Interfaces/ISpatialQueryable.cs
@@ -64,4 +64,4 @@
         T this[int index] { get; }
 
         /// <summary>
-        /// Gets the instances of <typeparamref name="T"/> with an index in
+        /// Gets the instances of <typeparamref name="T"/> with an index in <paramref name="indexes"/>. The index of an item to be retrieved should be found through the <see cref="NearestNeighborIndexes"/> or <see cref="R
