e8608614885e80adeeae1e94475fe4a5ca5eea5a
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-10-03 23:09:30 +0000

    split long lines

--- Supercluster/Structures/MTree/MNodeEntry.cs
+++ Supercluster/Structures/MTree/MNodeEntry.cs
@@ -1,4 +1,8 @@
 ﻿
 namespace Supercluster.MTree.NewDesign
 {
- 
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Represents a node entry. All node entries should belong to a node.
+    ///
