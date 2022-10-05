acec897f551e92c0523a6e3a8dcd2810be384ce9
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2022-10-05 09:02:29 +0000

    go fmt.

--- Supercluster/Structures/MTree/MNodeEntry.cs
+++ Supercluster/Structures/MTree/MNodeEntry.cs
@@ -5,4 +5,8 @@ namespace Supercluster.MTree.NewDesign
 
     /// <summary>
     /// Represents a node entry. All node entries should belong to a node.
-    ///
+    /// </summary>
+    /// <typeparam name="TValue">The </typeparam>
+    public class MNodeEntry<TValue>
+    {
+        private MNode<TValue> childNode = nu
