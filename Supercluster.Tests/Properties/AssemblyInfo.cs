556c8e1b634a30e9af93ff6c5551e19ece155462
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2023-07-21 06:58:48 +0000

    Version 0.10

--- Supercluster.Tests/Properties/AssemblyInfo.cs
+++ Supercluster.Tests/Properties/AssemblyInfo.cs
@@ -4,4 +4,8 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly
+// associated with an assembly.
+[assembly: AssemblyTitle("Supercluster.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
