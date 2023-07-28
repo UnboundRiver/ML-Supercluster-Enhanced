b59d6be351c22a3f7537edf1bd6a00270064e32d
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2023-07-28 08:47:45 +0000

    Fix issue #14

--- Supercluster.Tests/Structures/BoundablePriorityTest.cs
+++ Supercluster.Tests/Structures/BoundablePriorityTest.cs
@@ -12,4 +12,7 @@
         {
             var bp = new BoundablePriorityList<int, double>(3, true)
                          {
-                             
+                             { 34, 98744.90383 },
+                             { 23, 67.39030 },
+                             { 2, 2 },
+                             { 89, 3
