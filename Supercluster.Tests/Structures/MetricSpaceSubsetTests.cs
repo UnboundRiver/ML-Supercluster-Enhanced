dd108dd8c1ad2acb6107fdd41b91a7fc16d2f06b
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2023-11-17 06:57:36 +0000

    rule

--- Supercluster.Tests/Structures/MetricSpaceSubsetTests.cs
+++ Supercluster.Tests/Structures/MetricSpaceSubsetTests.cs
@@ -7,4 +7,10 @@ namespace Supercluster.Tests.Structures
 
     using NUnit.Framework;
 
- 
+    using Supercluster.Core;
+    using Supercluster.Structures;
+
+    using Utilities = Supercluster.Tests.Utilities;
+
+    [TestFixture]
+   
