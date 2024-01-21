8b50976118b20a724df1278c530bb363790dd23b
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2024-01-21 09:48:02 +0000

    Standardize and update code

--- Supercluster.VisualTests/Properties/Resources.Designer.cs
+++ Supercluster.VisualTests/Properties/Resources.Designer.cs
@@ -6,4 +6,11 @@
 //     Changes to this file may cause incorrect behavior and will be lost if
 //     the code is regenerated.
 // </auto-generated>
-//---------------------
+//------------------------------------------------------------------------------
+
+namespace Supercluster.VisualTests.Properties
+{
+
+
+    /// <summary>
+    ///   A strongly-typ
