d6aed4e4307db9cb1587da1c8b77926aa33e4d18
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2024-02-13 14:13:31 +0000

    Test cleanup.

--- Supercluster.VisualTests/Properties/Resources.Designer.cs
+++ Supercluster.VisualTests/Properties/Resources.Designer.cs
@@ -54,4 +54,5 @@ namespace Supercluster.VisualTests.Properties
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
-       
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableS
