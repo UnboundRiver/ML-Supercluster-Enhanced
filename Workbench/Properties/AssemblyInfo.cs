dd44d5766bd091f2dbba992cc5ec91a8cb17cec1
UnboundRiver <139232552+UnboundRiver@users.noreply.github.com>
2024-04-02 05:30:33 +0000

    updated contents

--- Workbench/Properties/AssemblyInfo.cs
+++ Workbench/Properties/AssemblyInfo.cs
@@ -3,4 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to mo
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Workbench")]
+[assembly: Assemb
